b7dc2c8422b45fee699eb7d019e1f07aac70d7f0
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2023-07-30 15:51:15 +0200

    Added Pole and top notch(called tip of the pole)

--- FlagHasting/Brick.swift
+++ FlagHasting/Brick.swift
@@ -6,48 +6,81 @@
 //
 
 import Foundation
-
 import SwiftUI
-/*
-struct BrickView: View {
-    var body: some View {
-        VStack(spacing: 0) {
-            Spacer() // Add spacer to push the bricks to the bottom
-            HStack(spacing: 0) {
-                ForEach(0..<6, id: \.self) { brickIndex in
-                    Brick()
-                        .frame(width: UIScreen.main.bounds.width / 6)
-                }
-            }
-        }
-        .frame(maxHeight: .infinity) // Make sure the VStack occupies the full height
+
+
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.closeSubpath()
+        return path
     }
 }
- 
- */
 
+
+struct Pole: View {
+    var body: some View {
+        let screenWidth = UIScreen.main.bounds.width
+        let poleWidth = screenWidth / 30 // Adjust the width of the pole
+        let poleHeight = UIScreen.main.bounds.height / 1.8 // Adjust the height of the pole
+        
+        VStack(spacing: 0) {
+            // Small triangle closing tip (finial) for the top of the pole
+            Triangle()
+                .fill(Color.brown)
+                .frame(width: poleWidth, height: poleWidth / 2) // Adjust the dimensions of the finial
+            
+            Rectangle()
+                .fill(Color.brown) // Color of the flagpole
+                .frame(width: poleWidth, height: poleHeight)
+        }
+    }
+}
 
 struct BrickView: View {
     var body: some View {
         VStack(spacing: 0) {
-            
+
             Spacer() // Add spacer to separate the layers
+            
+            Pole()
             
             // Second Layer of Bricks
             HStack(spacing: 0) {
-                HalfBrick() // Add the HalfBrick at the beginning of the second layer
+                //HalfBrick() // Add the HalfBrick at the beginning of the second layer
+                ForEach(0..<3, id: \.self) { brickIndex in
+                    Brick()
+                }
+            }
+
+            
+            // Second Layer of Bricks
+            HStack(spacing: 0) {
+                //HalfBrick() // Add the HalfBrick at the beginning of the second layer
+                ForEach(0..<4, id: \.self) { brickIndex in
+                    Brick()
+                }
+            }
+
+            // Second Layer of Bricks
+            HStack(spacing: 0) {
+                //HalfBrick() // Add the HalfBrick at the beginning of the second layer
                 ForEach(0..<5, id: \.self) { brickIndex in
                     Brick()
                 }
             }
-            
+
             // First Layer of Bricks
             HStack(spacing: 0) {
                 ForEach(0..<6, id: \.self) { brickIndex in
                     Brick()
                 }
             }
-            
+
         }
         .frame(maxHeight: .infinity) // Make sure the VStack occupies the full height
     }
@@ -59,7 +92,7 @@ struct HalfBrick: View {
         let brickW = screenWidth / 6 // Calculate brick width as 1/6 of the screen width
         
         Rectangle()
-            .fill(Color.clear) // Transparent background for half brick
+            .fill(Color.blue) // Transparent background for half brick
             .frame(width: brickW / 2, height: 25)
     }
 }
@@ -99,10 +132,25 @@ struct Brick: View {
     }
 }
 
+// Preview the BrickView with the correct layout
+//struct BrickView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ZStack {
+//                BrickView()
+//                // You can add more components or modify the stage layout here if needed.
+//        }
+//    }
+//}
 
+// Preview the BrickView with the correct layout
 struct BrickView_Previews: PreviewProvider {
     static var previews: some View {
-        BrickView()
+        ZStack {
+//            Pole()
+//                .offset(y: -UIScreen.main.bounds.height / 4)
+            BrickView()
+            // Position the pole above the center
+            // You can add more components or modify the stage layout here if needed.
+        }
     }
 }
-
